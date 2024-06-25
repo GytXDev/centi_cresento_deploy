e3f8adea352ebf4c9debed64b36c5a456372d163
GytXDev <mahelnguindja@gmail.com>
2024-06-25 17:16:39 +0100

    Fix import issues and update VoxelDog component

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7103258c9baad97a"; }
+/******/ 		__webpack_require__.h = function() { return "453b28835a4f27cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
